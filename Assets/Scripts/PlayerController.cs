6dbacfe2397a9350e131ebdf3d456d7854796bed
AlexSche <Alexander.S_97@gmx.de>
2025-10-10 14:21:57 +0200

    add flashlight placeholder

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(PlayerInput))]
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    private PlayerInput playerInput;
+    private InputAction flashlightAction;
+    private bool isFlashlightActive = true;
+    [SerializeField] private GameObject flashlightGameObject;
+
+    void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        flashlightAction = playerInput.actions.FindActionMap("Player").FindAction("Flashlight");
+        flashlightAction.started += SwitchFlashlight;
+    }
+    void OnEnable()
+    {
+        flashlightAction.Enable();
+    }
+
+    void OnDisable()
+    {
+        flashlightAction.Disable();
+    }
+
+    void SwitchFlashlight(InputAction.CallbackContext callbackContext)
+    {
+        isFlashlightActive = !isFlashlightActive;
+        flashlightGameObject.SetActive(isFlashlightActive);
+    }
+}
