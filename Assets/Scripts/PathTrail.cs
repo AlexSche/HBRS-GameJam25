ce57e21b5233406361e347994086feb6eca24404
AlexSche <Alexander.S_97@gmx.de>
2025-10-11 17:34:11 +0200

    improved level

--- Assets/Scripts/PathTrail.cs
+++ Assets/Scripts/PathTrail.cs
@@ -16,5 +16,22 @@ public class PathTrail : MonoBehaviour
         {
             lineRenderer.SetPosition(i, checkpoints[i].transform.position);
         }
+
+        DrawLitTrailToCheckpoints(4f);
+    }
+
+    public void DrawLitTrailToCheckpoints(float yOffset)
+    {
+        GameObject line = Instantiate(linePrefab);
+        LineRenderer lineRenderer = line.GetComponent<LineRenderer>();
+        lineRenderer.positionCount = checkpoints.Count;
+        lineRenderer.startWidth = 0.1f;
+        lineRenderer.endWidth = 0.1f;
+        for (int i = 0; i < checkpoints.Count; i++)
+        {
+            Vector3 offsetPosition = checkpoints[i].transform.position;
+            offsetPosition.y = offsetPosition.y + yOffset;
+            lineRenderer.SetPosition(i, offsetPosition);
+        }
     }
 }
