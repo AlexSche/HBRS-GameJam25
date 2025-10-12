a68f3664aadf50718b170099f5725868d9c76647
AlexSche <Alexander.S_97@gmx.de>
2025-10-12 14:33:55 +0200

    add ending

--- Assets/Scripts/Elevator.cs
+++ Assets/Scripts/Elevator.cs
@@ -1,13 +1,39 @@
+using System;
 using UnityEngine;
 
 public class Elevator : MonoBehaviour
 {
+    [SerializeField] Rigidbody rb;
+    [SerializeField] Transform endScenePosition;
+    [SerializeField] GameObject door;
     public bool isWorking = false;
     private bool isPlayerInElevator;
+    private bool isMoving = false;
+
 
     void Start()
     {
         PlayerEvents.OnPlayerInteract += StartElevator;
+    }
+
+    void FixedUpdate()
+    {
+        if (isMoving)
+        {
+            if (rb.position.y < 73)
+            {
+                rb.MovePosition(rb.position + Vector3.up * 5 * Time.fixedDeltaTime);
+            }
+            else
+            {
+                rb.MovePosition(rb.position + (endScenePosition.position - rb.position).normalized * 10 * Time.fixedDeltaTime);
+                if (Vector3.Distance(rb.position, endScenePosition.position) < 1)
+                {
+                    isMoving = false;
+                    door.SetActive(false);
+                }
+            }
+        }
     }
 
     void OnTriggerEnter(Collider collision)
@@ -31,6 +57,8 @@ public class Elevator : MonoBehaviour
         if (isWorking && isPlayerInElevator)
         {
             Debug.Log("Ending game!");
+            door.SetActive(true);
+            isMoving = true;
         }
     }
 }
